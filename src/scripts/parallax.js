1caeb1c0b1cca6a762192a82fa38d1d2830c5530
Yevhen Oksenchuk <oksenchuk.yevhen@gmail.com>
2019-05-21 15:01:15 +0300

    add section program

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,8 +1,13 @@
 (function () {
 
     function parall(e) {
-        let layer = this.querySelector('.parallax__layer');
-        layer.style.transform = `translate(${event.clientX/50}px, ${event.clientY/20}px)`
+        let hero = document.querySelector('.hero__container');
+        let title = document.querySelector('.hero__title');
+        let layer = document.querySelector('.parallax__layer');
+        if (e.target == layer || e.target == hero || e.target == title) {
+            
+            layer.style.transform = `translate(${event.clientX/50}px, ${event.clientY/20}px)`
+        }
     }
     
 
